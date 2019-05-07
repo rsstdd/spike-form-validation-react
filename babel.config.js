489253fef7cd087215f95f8edac8471967b8ba8a
Ross <rosstodd@lululemon.com>
2019-05-07 16:08:23 -0700

    spike (form validation): WIP - Update Babelrc

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-    '@babel/plugin-transform-async-to-generator',
-    '@babel/plugin-transform-runtime',
-    [('@babel/plugin-proposal-class-properties', { loose: true })],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    `@babel/plugin-transform-runtime`,
   ],
 }
